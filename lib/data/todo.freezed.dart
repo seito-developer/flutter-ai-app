b022440da523db396aa971257a60cac8efaed9b9
Seito Horiguchi <seito.horiguchi@gmail.com>
2023-10-25 21:57:13 +0900

    changed to todo-app

---
+++ lib/data/todo.freezed.dart
@@ -0,0 +1,266 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'todo.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+Todo _$TodoFromJson(Map<String, dynamic> json) {
+  return _Todo.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Todo {
+  String get id => throw _privateConstructorUsedError;
+  String get title => throw _privateConstructorUsedError;
+  bool get isDone => throw _privateConstructorUsedError;
+  int get colorNo => throw _privateConstructorUsedError;
+  DateTime? get deadlineTime => throw _privateConstructorUsedError;
+  DateTime get createdTime => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $TodoCopyWith<Todo> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $TodoCopyWith<$Res> {
+  factory $TodoCopyWith(Todo value, $Res Function(Todo) then) =
+      _$TodoCopyWithImpl<$Res, Todo>;
+  @useResult
+  $Res call(
+      {String id,
+      String title,
+      bool isDone,
+      int colorNo,
+      DateTime? deadlineTime,
+      DateTime createdTime});
+}
+
+/// @nodoc
+class _$TodoCopyWithImpl<$Res, $Val extends Todo>
+    implements $TodoCopyWith<$Res> {
+  _$TodoCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? title = null,
+    Object? isDone = null,
+    Object? colorNo = null,
+    Object? deadlineTime = freezed,
+    Object? createdTime = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      isDone: null == isDone
+          ? _value.isDone
+          : isDone // ignore: cast_nullable_to_non_nullable
+              as bool,
+      colorNo: null == colorNo
+          ? _value.colorNo
+          : colorNo // ignore: cast_nullable_to_non_nullable
+              as int,
+      deadlineTime: freezed == deadlineTime
+          ? _value.deadlineTime
+          : deadlineTime // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+      createdTime: null == createdTime
+          ? _value.createdTime
+          : createdTime // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$TodoImplCopyWith<$Res> implements $TodoCopyWith<$Res> {
+  factory _$$TodoImplCopyWith(
+          _$TodoImpl value, $Res Function(_$TodoImpl) then) =
+      __$$TodoImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String id,
+      String title,
+      bool isDone,
+      int colorNo,
+      DateTime? deadlineTime,
+      DateTime createdTime});
+}
+
+/// @nodoc
+class __$$TodoImplCopyWithImpl<$Res>
+    extends _$TodoCopyWithImpl<$Res, _$TodoImpl>
+    implements _$$TodoImplCopyWith<$Res> {
+  __$$TodoImplCopyWithImpl(_$TodoImpl _value, $Res Function(_$TodoImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? title = null,
+    Object? isDone = null,
+    Object? colorNo = null,
+    Object? deadlineTime = freezed,
+    Object? createdTime = null,
+  }) {
+    return _then(_$TodoImpl(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      isDone: null == isDone
+          ? _value.isDone
+          : isDone // ignore: cast_nullable_to_non_nullable
+              as bool,
+      colorNo: null == colorNo
+          ? _value.colorNo
+          : colorNo // ignore: cast_nullable_to_non_nullable
+              as int,
+      deadlineTime: freezed == deadlineTime
+          ? _value.deadlineTime
+          : deadlineTime // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+      createdTime: null == createdTime
+          ? _value.createdTime
+          : createdTime // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$TodoImpl with DiagnosticableTreeMixin implements _Todo {
+  const _$TodoImpl(
+      {required this.id,
+      required this.title,
+      required this.isDone,
+      required this.colorNo,
+      required this.deadlineTime,
+      required this.createdTime});
+
+  factory _$TodoImpl.fromJson(Map<String, dynamic> json) =>
+      _$$TodoImplFromJson(json);
+
+  @override
+  final String id;
+  @override
+  final String title;
+  @override
+  final bool isDone;
+  @override
+  final int colorNo;
+  @override
+  final DateTime? deadlineTime;
+  @override
+  final DateTime createdTime;
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return 'Todo(id: $id, title: $title, isDone: $isDone, colorNo: $colorNo, deadlineTime: $deadlineTime, createdTime: $createdTime)';
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty('type', 'Todo'))
+      ..add(DiagnosticsProperty('id', id))
+      ..add(DiagnosticsProperty('title', title))
+      ..add(DiagnosticsProperty('isDone', isDone))
+      ..add(DiagnosticsProperty('colorNo', colorNo))
+      ..add(DiagnosticsProperty('deadlineTime', deadlineTime))
+      ..add(DiagnosticsProperty('createdTime', createdTime));
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$TodoImpl &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.title, title) || other.title == title) &&
+            (identical(other.isDone, isDone) || other.isDone == isDone) &&
+            (identical(other.colorNo, colorNo) || other.colorNo == colorNo) &&
+            (identical(other.deadlineTime, deadlineTime) ||
+                other.deadlineTime == deadlineTime) &&
+            (identical(other.createdTime, createdTime) ||
+                other.createdTime == createdTime));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType, id, title, isDone, colorNo, deadlineTime, createdTime);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$TodoImplCopyWith<_$TodoImpl> get copyWith =>
+      __$$TodoImplCopyWithImpl<_$TodoImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$TodoImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Todo implements Todo {
+  const factory _Todo(
+      {required final String id,
+      required final String title,
+      required final bool isDone,
+      required final int colorNo,
+      required final DateTime? deadlineTime,
+      required final DateTime createdTime}) = _$TodoImpl;
+
+  factory _Todo.fromJson(Map<String, dynamic> json) = _$TodoImpl.fromJson;
+
+  @override
+  String get id;
+  @override
+  String get title;
+  @override
+  bool get isDone;
+  @override
+  int get colorNo;
+  @override
+  DateTime? get deadlineTime;
+  @override
+  DateTime get createdTime;
+  @override
+  @JsonKey(ignore: true)
+  _$$TodoImplCopyWith<_$TodoImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,28 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-import 'firebase_options.dart';
+import 'data/todo.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Todo Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSwatch(
+          brightness: Brightness.light,
+          primarySwatch: Colors.lightBlue,
+        ),
+        scaffoldBackgroundColor: const Color.fromRGBO(0xF2, 0xF2, 0xF2, 1.0),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Todo'),
     );
   }
 }
@@ -38,52 +37,206 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final _collection = FirebaseFirestore.instance.collection('counter');
-  void _incrementCounter() async {
-    final currentData = await _collection.doc('data').get();
-    final counter = currentData['data'] as int;
-    _collection.doc('data').update({'data': counter + 1});
-  }
+  static List<Color> colors = [
+    Colors.white.withOpacity(0.8),
+    Colors.red.withOpacity(0.2),
+    Colors.green.withOpacity(0.2),
+    Colors.blue.withOpacity(0.2),
+    Colors.yellow.withOpacity(0.2),
+    Colors.cyan.withOpacity(0.2),
+  ];
+
+  String _title = '';
+  int _selectedColor = 0;
+  DateTime? _selectedDateTime = DateTime.now();
+  final formatDate = DateFormat('yyyy/MM/dd');
+
+  List<Todo> todo = [
+    Todo(
+      id: '1',
+      title: 'あいうえお',
+      isDone: false,
+      colorNo: 0,
+      deadlineTime: DateTime(2023, 5, 5),
+      createdTime: DateTime(2023, 1, 5),
+    ),
+    Todo(
+      id: '2',
+      title: 'かきくけこ',
+      isDone: true,
+      colorNo: 1,
+      deadlineTime: DateTime(2023, 5, 5),
+      createdTime: DateTime(2023, 1, 5),
+    ),
+    Todo(
+      id: '3',
+      title: 'さしすせそ',
+      isDone: false,
+      colorNo: 2,
+      deadlineTime: DateTime(2023, 5, 5),
+      createdTime: DateTime(2023, 1, 5),
+    )
+  ];
+
+  static const circularEdge = Radius.circular(16.0);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ListView.builder(
+                  itemCount: todo.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: colors[todo[index].colorNo],
+                        borderRadius: BorderRadius.vertical(
+                          top: index == 0 ? circularEdge : Radius.zero,
+                          bottom: index == todo.length - 1
+                              ? circularEdge
+                              : Radius.zero,
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 8.0, vertical: 4.0),
+                        child: Row(
+                          children: [
+                            Transform.scale(
+                              scale: 1.5,
+                              child: Checkbox(
+                                value: todo[index].isDone,
+                                shape: const CircleBorder(),
+                                onChanged: (isChecked) =>
+                                    _onChangeIsDone(index, isChecked),
+                              ),
+                            ),
+                            Text(
+                              todo[index].title,
+                              style: Theme.of(context).textTheme.titleMedium,
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
             ),
-            StreamBuilder(
-                stream: _collection.snapshots(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasError) {
-                    return Text(snapshot.error.toString());
-                  }
-                  if (!snapshot.hasData) {
-                    return const CircularProgressIndicator();
-                  }
-
-                  final data = snapshot.data!;
-                  final counter = data.docs[0]['data'];
-
-                  return Text(
-                    '$counter',
-                    style: Theme.of(context).textTheme.headline4,
-                  );
-                })
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: _onAddTodo,
+        tooltip: 'add todo',
         child: const Icon(Icons.add),
-      ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void _onAddTodo() {
+    _selectedColor = 0;
+    _selectedDateTime = null;
+    _title = '';
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Column(
+          children: [
+            TextField(
+              onChanged: (value) => _title = value,
+            ),
+            StatefulBuilder(builder: (context, setStateInSheet) {
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  OutlinedButton(
+                    child: _selectedDateTime == null
+                        ? Row(
+                            children: const [
+                              Icon(Icons.calendar_month),
+                              Text('期限'),
+                            ],
+                          )
+                        : Text(formatDate.format(_selectedDateTime!)),
+                    onPressed: () async {
+                      final today = DateTime.now();
+                      final date = await showDatePicker(
+                        context: context,
+                        initialDate: today,
+                        firstDate: today.subtract(const Duration(days: 10)),
+                        lastDate: today.add(const Duration(days: 100)),
+                      );
+                      if (date != null) {
+                        setStateInSheet(() {
+                          _selectedDateTime = date;
+                        });
+                      }
+                    },
+                  ),
+                  for (int i = 0; i < colors.length; i++)
+                    CircleAvatar(
+                      backgroundColor: Colors.black,
+                      radius: 16.0,
+                      child: Transform.scale(
+                        scale: 1.5,
+                        child: Radio<int>(
+                          value: i,
+                          groupValue: _selectedColor,
+                          fillColor: MaterialStateProperty.all(
+                              colors[i].withOpacity(1.0)),
+                          onChanged: (int? index) {
+                            if (index != null) {
+                              setStateInSheet(() {
+                                _selectedColor = index;
+                              });
+                            }
+                          },
+                        ),
+                      ),
+                    ),
+                  ElevatedButton(
+                      onPressed: _title.isEmpty
+                          ? null
+                          : () {
+                              final newTodo = Todo(
+                                id: '',
+                                title: _title,
+                                isDone: false,
+                                colorNo: _selectedColor,
+                                deadlineTime: _selectedDateTime,
+                                createdTime: DateTime.now(),
+                              );
+
+                              setState(() {
+                                todo.add(newTodo);
+                              });
+
+                              Navigator.pop(context);
+                            },
+                      child: const Text('追加')),
+                ],
+              );
+            }),
+          ],
+        );
+      },
+    );
+  }
+
+  void _onChangeIsDone(int index, bool? isChecked) {
+    if (isChecked == null) {
+      return;
+    }
+    setState(() {
+      todo[index] = todo[index].copyWith(isDone: isChecked);
+    });
   }
 }

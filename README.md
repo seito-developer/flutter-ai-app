473105f84605204901fa447a81a69dd33fa78c38
Seito Horiguchi <seito.horiguchi@gmail.com>
2023-10-25 20:51:42 +0900

    reinit

--- README.md
+++ README.md
@@ -1,37 +1,16 @@
-# 【Flutter】Firestore 実践ガイド
-「[【Flutter】Firestore 実践ガイド](https://flutter.salon/firestore)」コースのソースコード
+# flutter_ai_app
 
-## 説明
-- 本リポジトリはUdemyの「[【Flutter】Firestore 実践ガイド](https://flutter.salon/firestore)」コースで扱うソースコードを管理するためのものです。受講されていない方は、上記のクーポン付きリンクから受講していただけると嬉しいです。
-- コース内でレクチャー終了のコードをブランチで分けているときがあります(レクチャーの番号とブランチの番号は一致 **しません**)
-- ご不明な点がありましたら、遠慮なくUdemyのQ&Aでお問い合わせください。
+A new Flutter project.
 
-## 実行
-### FlutterFireの準備
-コースのセクション2をご覧下さい(プレビューで無料で見られます)
+## Getting Started
 
-### Flutterの準備
-```
-git clone https://github.com/sakushin-udemy/firebase_todo_list.git
-cd firebase_todo_list
-flutter pub get
-flutter pub run build_runner build
-flutterfire configure
-```
-### Firebaseの準備
-- Firestoreの作成
-- Authenticationの作成
-メール/パスワードを有効(メールリンクは、無効)
+This project is a starting point for a Flutter application.
 
+A few resources to get you started if this is your first Flutter project:
 
-## 動画コース一覧
-他にも以下のようなコースをリリースしております。
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
 
-| タイトル |  概要 | 
-| ---- | ---- |  
-|[Flutter x Riverpod x MVVMで実現するシンプルな設計(Firebase, WebAPI対応)](https://flutter.salon/riverpod)|Riverpodの基本的な使用方法から始まり、Riverpodを使用して、MVVMパターンを実現し、ロジックとビューを切り離し、メンテしやすい実装方法を学習します。  |
-|[【Flutter】Firestore 実践ガイド](https://flutter.salon/firestore)|FlutterでFirestoreの導入から始まり、をTDD(テスト駆動型開発、テストファースト)の考えの基、Firestoreなしで実装をしていきます。 |
-|[FlutterでのはじめてのTDD(テスト 駆動開発)！](https://flutter.salon/tdd)|アプリの品質を保つために、自動でテストができて、アプリの品質を担保する切り札、それがテストファースト、テスト駆動開発を学びます |
-|[60分で分かるFlutter Golden Tests (golden_toolkit by ebay)](https://flutter.salon/gt)| Flutterアプリの動作確認の目視確認を辞めるためにeBayが開発した「Flutter Golden Tests」を学びます  |
-|[【Flutter】最短で75言語に対応させ、あなたのアプリを世界の人が届ける方法](https://flutter.salon/l10n)|あなたのFlutterアプリの国際化対応から開始して、大量の言語ファイルを作るためのプログラムも公開してます。あなたのアプリを世界中に配信するためのコストを大幅に削減します。 |
-[さくしんのUdemy講座一覧](https://www.udemy.com/user/sakura-21/)
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_todo_list/login_dialog.dart';
-import 'package:firebase_todo_list/repositories/authentication_repository.dart';
-import 'package:firebase_todo_list/repositories/todo_repository.dart';
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
-import 'package:intl/intl.dart';
-
-import 'data/todo.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -15,12 +9,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
-  // DIの設定
-  GetIt.I.registerLazySingleton<FirebaseFirestore>(
-      () => FirebaseFirestore.instance);
-
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -28,325 +17,79 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Todo Demo',
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSwatch(
-          brightness: Brightness.light,
-          primarySwatch: Colors.lightBlue,
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         ),
-        scaffoldBackgroundColor: const Color.fromRGBO(0xF2, 0xF2, 0xF2, 1.0),
-        dialogBackgroundColor: const Color.fromRGBO(0xF2, 0xF2, 0xF2, 1.0),
+        home: MyHomePage(),
       ),
-      home: const MyHomePage(title: 'Flutter Todo'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  static List<Color> colors = [
-    Colors.white.withOpacity(0.8),
-    Colors.red.withOpacity(0.2),
-    Colors.green.withOpacity(0.2),
-    Colors.blue.withOpacity(0.2),
-    Colors.yellow.withOpacity(0.2),
-    Colors.cyan.withOpacity(0.2),
-  ];
-
-  String _title = '';
-  int _selectedColor = 0;
-  DateTime? _selectedDateTime = DateTime.now();
-  final formatDate = DateFormat('yyyy/MM/dd');
-
-  static const circularEdge = Radius.circular(16.0);
-
-  bool _visibleDoneItem = false;
-  bool _descending = false;
-
-  final _auth = AuthenticationRepository();
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-        stream: _auth.uid(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData || snapshot.hasError) {
-            return const CircularProgressIndicator();
-          }
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
-          final userId = snapshot.data!;
-          if (userId.isEmpty) {
-            WidgetsBinding.instance.addPostFrameCallback((_) {
-              showDialog(
-                  context: context, builder: (context) => const LoginDialog());
-            });
-            return const CircularProgressIndicator();
-          }
-
-          // ログインしている
-          final todoRepository = TodoRepository(userId);
-
-          return Scaffold(
-            appBar: AppBar(),
-            body: Center(
-                child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: OutlinedButton(
-                    onPressed: () => _auth.signOut(),
-                    child: const Text('ログアウト'),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        Transform.scale(
-                          scale: 1.5,
-                          child: Checkbox(
-                              value: _visibleDoneItem,
-                              onChanged: (value) {
-                                if (value == null) {
-                                  return;
-                                }
-                                setState(() {
-                                  _visibleDoneItem = value;
-                                });
-                              }),
-                        ),
-                        const Text('実施済みも表示'),
-                      ],
-                    ),
-                    ElevatedButton(
-                        onPressed: () {
-                          setState(() {
-                            _descending = !_descending;
-                          });
-                        },
-                        child: Text(_descending ? '締切 遅い' : '締切 早い'))
-                  ],
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: StreamBuilder(
-                        stream: todoRepository
-                            .stream(
-                              isDone: _visibleDoneItem ? null : false,
-                              sortMethod: SortMethod.deadlineTime,
-                              descending: _descending,
-                            )
-                            .snapshots(),
-                        builder: (context, snapshot) {
-                          if (snapshot.hasError) {
-                            return Text('Error: ${snapshot.error}');
-                          } else if (!snapshot.hasData) {
-                            return const CircularProgressIndicator();
-                          }
-
-                          final todo = snapshot.data!.docs
-                              .map((e) => Todo.fromJson(e.data()))
-                              .toList();
-                          return ListView.builder(
-                            itemCount: todo.length,
-                            itemBuilder: (BuildContext context, int index) {
-                              return GestureDetector(
-                                onLongPress: () => _onTodoLongPressed(
-                                    todoRepository, todo[index]),
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    color: colors[todo[index].colorNo],
-                                    borderRadius: BorderRadius.vertical(
-                                      top: index == 0
-                                          ? circularEdge
-                                          : Radius.zero,
-                                      bottom: index == todo.length - 1
-                                          ? circularEdge
-                                          : Radius.zero,
-                                    ),
-                                  ),
-                                  child: Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 8.0, vertical: 4.0),
-                                    child: Row(
-                                      children: [
-                                        Transform.scale(
-                                          scale: 1.5,
-                                          child: Checkbox(
-                                            value: todo[index].isDone,
-                                            shape: const CircleBorder(),
-                                            onChanged: (isChecked) =>
-                                                _onChangeIsDone(todoRepository,
-                                                    todo[index], isChecked),
-                                          ),
-                                        ),
-                                        Text(
-                                          todo[index].title,
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .titleMedium,
-                                        ),
-                                        Expanded(child: Container()),
-                                        Text(
-                                          todo[index].deadlineTime == null
-                                              ? ''
-                                              : formatDate.format(
-                                                  todo[index].deadlineTime!),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              );
-                            },
-                          );
-                        }),
-                  ),
-                ),
-              ],
-            )),
-
-            floatingActionButton: FloatingActionButton(
-              onPressed: () => _onAddTodo(todoRepository),
-              tooltip: 'add todo',
-              child: const Icon(Icons.add),
-            ), // This trailing comma makes auto-formatting nicer for build methods.
-          );
-        });
-  }
-
-  void _onAddTodo(TodoRepository todoRepository) {
-    _selectedColor = 0;
-    _selectedDateTime = null;
-    _title = '';
-    showModalBottomSheet(
-      context: context,
-      builder: (BuildContext context) {
-        return Column(
-          children: [
-            TextField(
-              onChanged: (value) => _title = value,
-            ),
-            StatefulBuilder(builder: (context, setStateInSheet) {
-              return Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  OutlinedButton(
-                    child: _selectedDateTime == null
-                        ? Row(
-                            children: const [
-                              Icon(Icons.calendar_month),
-                              Text('期限'),
-                            ],
-                          )
-                        : Text(formatDate.format(_selectedDateTime!)),
-                    onPressed: () async {
-                      final today = DateTime.now();
-                      final date = await showDatePicker(
-                        context: context,
-                        initialDate: today,
-                        firstDate: today.subtract(const Duration(days: 10)),
-                        lastDate: today.add(const Duration(days: 100)),
-                      );
-                      if (date != null) {
-                        setStateInSheet(() {
-                          _selectedDateTime = date;
-                        });
-                      }
-                    },
-                  ),
-                  for (int i = 0; i < colors.length; i++)
-                    CircleAvatar(
-                      backgroundColor: Colors.black,
-                      radius: 16.0,
-                      child: Transform.scale(
-                        scale: 1.5,
-                        child: Radio<int>(
-                          value: i,
-                          groupValue: _selectedColor,
-                          fillColor: MaterialStateProperty.all(
-                              colors[i].withOpacity(1.0)),
-                          onChanged: (int? index) {
-                            if (index != null) {
-                              setStateInSheet(() {
-                                _selectedColor = index;
-                              });
-                            }
-                          },
-                        ),
-                      ),
-                    ),
-                  ElevatedButton(
-                      onPressed: _title.isEmpty
-                          ? null
-                          : () async {
-                              final newTodo = Todo(
-                                id: '',
-                                title: _title,
-                                isDone: false,
-                                colorNo: _selectedColor,
-                                deadlineTime: _selectedDateTime,
-                                createdTime: DateTime.now(),
-                              );
-                              todoRepository.add(newTodo);
-                              if (context.mounted) {
-                                Navigator.pop(context);
-                              }
-                            },
-                      child: const Text('追加')),
-                ],
-              );
-            }),
-          ],
-        );
-      },
-    );
-  }
-
-  void _onChangeIsDone(
-      TodoRepository todoRepository, Todo todo, bool? isChecked) {
-    if (isChecked == null) {
-      return;
-    }
-
-    final newData = todo.copyWith(isDone: isChecked);
-    todoRepository.update(newData);
-  }
-
-  void _onTodoLongPressed(TodoRepository todoRepository, Todo todo) async {
-    var result = await showDialog<bool>(
-      context: context,
-      builder: (BuildContext context) => AlertDialog(
-        title: const Text('確認'),
-        content: Text('「${todo.title}」を削除します'),
-        actions: <Widget>[
-          SimpleDialogOption(
-            child: const Text('キャンセル'),
-            onPressed: () => Navigator.of(context).pop(false),
-          ),
-          SimpleDialogOption(
-            child: const Text('削除'),
-            onPressed: () => Navigator.of(context).pop(true),
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('A random AWESOME idea:'),
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () {
+              appState.getNext();
+            },
+            child: Text('Next'),
           ),
         ],
       ),
     );
+  }
+}
 
-    if (result == true) {
-      todoRepository.delete(todo.id);
-    }
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context); // ← Add this.
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary, // ← And also this.
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
+      ),
+    );
   }
 }
